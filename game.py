0bfae72320793f21da4496cca70504fd1d4b7fef
Ultraman <totobriac@gmail.com>
2019-12-15 21:16:08 +0100

    create maze good way

---
+++ game.py
@@ -0,0 +1,28 @@
+walls = []
+paths = []
+
+class Maze :
+
+    def __init__ (self, level):
+        self.level = level
+
+    def create_maze (self):
+        with open (self.level) as f:
+            for x, line in enumerate (f):
+                for y, c in enumerate (line):
+                    if c == 'x':
+                        walls.append((x,y))
+                    elif c == '0':
+                        paths.append((x,y))
+
+maze = Maze ('maze.txt')
+
+maze.create_maze()
+
+
+
+position = (1,1)
+
+if position in walls:
+    print ('ok')
+else: print('no')
