cbbca586736d9a67de0d8bcea36e193eb8b59347
Ultraman <totobriac@gmail.com>
2019-12-18 23:53:17 +0100

    Fully playable in console :)

--- classes.py
+++ classes.py
@@ -9,9 +9,8 @@ class Maze :
         self.paths = []
         self.guard_position = []
         self.hero_position = []
-
+     
     def create_maze (self):
-
         
         with open (self.level) as f:
             for y, line in enumerate (f,1):                       
@@ -19,13 +18,16 @@ class Maze :
                     if c == 'x': self.walls.append([x,y])
                     elif c == '0': self.paths.append([x,y])
                     elif c == 'G': self.paths.append([x,y]), self.guard_position.append([x,y])
-                    elif c == 'H': self.paths.append([x,y]), self.hero_position.append([x,y]) 
-                        
-            
+                    elif c == 'H': self.paths.append([x,y]), self.hero_position.append([x,y])                   
             return self.walls, self.paths, self.guard_position, self.hero_position
+
     def items (self):
 
-        items_position = random.choices(self.paths[1:-1], k = 3)
+        items_position = []      
+        while len(items_position) < 3:
+            item_position = random.choice(self.paths[1:-1])
+            if item_position not in items_position and item_position not in self.guard_position and item_position not in self.hero_position:
+                items_position.append(item_position)
         return items_position
 
 
@@ -36,6 +38,7 @@ class Player :
 
         self.name = name
         self.position = position
+        self.collected_items = []
         
     def calculate_move(self, direction):
         
@@ -49,4 +52,17 @@ class Player :
     def do_move(self, direction):
         
         self.position = self.calculate_move(direction)
+
+    def collect (self, position):
+        
+        if position not in self.collected_items:            
+            self.collected_items.append(position)
+            print ('You get an object!')  
+            return self.collected_items 
+
+    def win (self, collected_items):
+
+        if len(collected_items) == 3: print ('Good job!!!')
+        else: print ('Dead!!!')
+
 
--- game.py
+++ game.py
@@ -1,27 +1,27 @@
 from classes import *
 import random
 
-
-maze = Maze ('maze.txt')
+maze = Maze('maze.txt')
 maze.create_maze()
-objects = maze.items ()
+objects = maze.items()
 print (objects)
-
 
 position = [maze.hero_position[0][0], maze.hero_position[0][1]]
 mac = Player('mac',position)
-
 
 keep_playing = True
 
 while keep_playing:
     print (mac.position)
+    print (mac.collected_items)
     direction = input("Where are you heading to?")
     if direction == "quit":
         keep_playing = False
     elif mac.calculate_move(direction) in maze.paths:
-        mac.do_move(direction)        
+        mac.do_move(direction)
         print('ok')
-        if mac.position in maze.guard_position:
-            print('It is a win')   
+        if mac.position in objects:                    
+            mac.collect(mac.position)                        
+        elif mac.position in maze.guard_position:
+            mac.win (mac.collected_items) 
     else : print ('No way!!')  
