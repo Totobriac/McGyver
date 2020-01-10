b56f0e9578a62f160616fcd85d88fcea273edca8
Toto Briac <totobriac@gmail.com>
2020-01-10 08:28:30 +0100

    all set

--- game_engine.py
+++ game_engine.py
@@ -7,13 +7,13 @@ class GameEngine:
 
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode((450, 450))  
+        self.window = pygame.display.set_mode((450, 450))
         self.maze = Maze('arts/maze.txt', self.window)
         self.maze.create_maze()
         self.maze.draw(self.window)
-        self.mac = Player('mac', self.maze.start_position, self.window)
-        self.mac.draw_player(self.window)
-        self.collected = self.mac.collected_items
+        self.player = Player('mac', self.maze.start_position, self.window)
+        self.player.draw_player(self.window)
+        self.collected = self.player.collected_items
         self.maze.items_placement()
         self.maze.items_draw(self.window, self.collected)
 
@@ -21,11 +21,11 @@ class GameEngine:
         self.maze.draw(self.window)
         self.maze.items_draw(self.window, self.collected)
         self.maze.text_items(self.collected)
-        self.mac.draw_player(self.window)
+        self.player.draw_player(self.window)
 
     def game_loop(self):
         for event in pygame.event.get():
- 
+
             if event.type == pygame.QUIT:
                 raise SystemExit
 
@@ -42,16 +42,15 @@ class GameEngine:
                 """
                 Checks if move is posible or not.
                 """
-                if self.mac.calculate_move(direction) in self.maze.paths:
-                    self.mac.do_move(direction)
+                if self.player.calculate_move(direction) in self.maze.paths:
+                    self.player.do_move(direction)
                     '''
                     Collects the objects
                     '''
-                    if self.mac.position in self.maze.items_position:
-                        self.mac.collect(self.mac.position)
+                    if self.player.position in self.maze.items_position:
+                        self.player.collect(self.player.position)
                     '''
                     Checks if the player win or lose.
                     '''
-                    if self.mac.position in self.maze.guard_position:
-                        self.mac.win(self.mac.collected_items)
-        
+                    if self.player.position in self.maze.guard_position:
+                        self.player.win(self.player.collected_items)
